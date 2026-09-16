47c24567ceaafd1acafb077c5dece942bf973e59
mry0220 <Kmm2559322@stu.o-hara.ac.jp>
2026-09-16 16:04:36 +0900

    Update FuelManager.cs

--- Assets/Code/Scripts/FuelManager.cs
+++ Assets/Code/Scripts/FuelManager.cs
@@ -2,15 +2,8 @@ using UnityEngine;
 
 public class FuelManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+   private void start()
     {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
